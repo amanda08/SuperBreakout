749be621f2f12facda7b65a27ed527ec140922f0
S <s>
2013-06-28 22:06:23 +1000

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -162,9 +162,9 @@ public class Breakout extends GraphicsProgram {
 		add(label);
 		while(true) {
 			label.setVisible(false);
-			pause(10);
+			pause(1000);
 			label.setVisible(true);
-			pause(50);
+			pause(1000);
 		}
 		
 		
