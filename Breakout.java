20b575e69b1d2ee412e738be0ef2c5b7c6dfefc7
S <s>
2013-06-29 12:59:02 +1000

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -154,7 +154,7 @@ public class Breakout extends GraphicsProgram {
  * RED, ORANGE, YELLOW, GREEN, CYAN.
  */
 	private void drawBricks() {
-		setBackground(Color.BLACK);
+		setBackground(Color.PINK);
 		int y = BRICK_Y_OFFSET;
 		drawRow(y,Color.RED);
 		y += BRICK_SEP + BRICK_HEIGHT;
