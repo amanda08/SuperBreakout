37d86f8726704b1468bf5fc31541f1fa34578cf3
S <s>
2013-06-29 12:43:29 +1000

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -33,7 +33,7 @@ public class Breakout extends GraphicsProgram {
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
 /** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 200;
+	private static final int PADDLE_WIDTH = 250;
 	private static final int PADDLE_HEIGHT = 10;
 
 /** Offset of the paddle up from the bottom */
