a4774ff0aaede1668126c6c0b26584e9b391a6b9
S <s>
2013-07-08 16:15:42 +1000

    {"type":"TI", "runs":"1"}

--- SuperBreakout.java
+++ SuperBreakout.java
@@ -76,7 +76,7 @@ public class SuperBreakout extends GraphicsProgram {
 	private static final int BRICK_Y_OFFSET = 70;
 
 /** Number of turns */
-	private static final int NTURNS = 3;
+	private static final int NTURNS = 6;
 
 /** Initializes the Breakout game */
 	public void init() {
